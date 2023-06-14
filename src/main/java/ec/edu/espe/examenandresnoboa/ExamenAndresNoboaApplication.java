f41d21f6ca1b8081efe9627fb62e2202917b29de
Andres Noboa V <aenoboa1@espe.edu.ec>
2023-06-14 17:01:20 -0500

    Initial commit

---
+++ src/main/java/ec/edu/espe/examenandresnoboa/ExamenAndresNoboaApplication.java
@@ -0,0 +1,13 @@
+package ec.edu.espe.examenandresnoboa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamenAndresNoboaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamenAndresNoboaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ec/edu/espe/examenandresnoboa/ExamenAndresNoboaApplicationTests.java
@@ -0,0 +1,13 @@
+package ec.edu.espe.examenandresnoboa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamenAndresNoboaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
